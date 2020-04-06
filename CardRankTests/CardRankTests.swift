bfacd5f0192974802b3e3a3189d7759e498596a0
Oscar Hernandez <oscar.hernandez@midtronics.com>
2020-04-06 10:56:32 -0600

    Added raw value to card value enum. Added comparable, equatable to hand and card. Added test to equal hands

--- CardRankTests/CardRankTests.swift
+++ CardRankTests/CardRankTests.swift
@@ -16,25 +16,55 @@ enum Suit {
     case club
 }
 
-enum Value {
-    case ace
-    case two
-    case three
-    case four
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case ten
-    case jack
-    case queen
-    case king
+enum Value : Int {
+    case ace = 1
+    case two = 2
+    case three = 3
+    case four = 4
+    case five = 5
+    case six = 6
+    case seven = 7
+    case eight = 8
+    case nine = 9
+    case ten = 10
+    case jack = 11
+    case queen = 12
+    case king = 13
 }
 
 struct Card {
     let value : Value
     let suit : Suit
+}
+
+extension Card : Comparable, Equatable {
+    static func < (lhs: Card, rhs: Card) -> Bool {
+        return lhs.value.rawValue < rhs.value.rawValue
+    }
+    
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return lhs.value.rawValue == rhs.value.rawValue
+    }
+}
+
+struct Hand  {
+    let cards : [Card]
+    
+    init(cards: [Card]) {
+        self.cards = cards.sorted(by: { $0 > $1 })
+    }
+}
+
+extension Hand : Equatable {
+    static func == (lhs: Hand, rhs: Hand) -> Bool {
+        return lhs.cards == rhs.cards
+    }
+}
+
+class Comparator {
+    static func compare(hand: Hand, to toHand: Hand) -> Hand {
+        return Hand(cards: [])
+    }
 }
 
 class CardRankTests: XCTestCase {
@@ -54,4 +84,27 @@ class CardRankTests: XCTestCase {
         XCTAssertEqual(card.suit, Suit.heart)
     }
     
+    func testHandsAreEqual() {
+        let firstHand = Hand(cards: [
+            Card(value: .ace, suit: .heart),
+            Card(value: .ace, suit: .club),
+            Card(value: .ten, suit: .diamond),
+            Card(value: .two, suit: .club),
+            Card(value: .five, suit: .spade)
+        ])
+        
+        let secondHand = Hand(cards: [
+            Card(value: .ten, suit: .club),
+            Card(value: .two, suit: .spade),
+            Card(value: .ace, suit: .diamond),
+            Card(value: .five, suit: .heart),
+            Card(value: .ace, suit: .spade),
+        ])
+        
+        print(firstHand.cards)
+        print("..")
+        print(secondHand.cards)
+        
+        XCTAssertEqual(firstHand, secondHand)
+    }
 }
