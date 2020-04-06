908570c0551962388eb8b548052c8315f9dd5f2d
Oscar Hernandez <oscar.hernandez@midtronics.com>
2020-04-06 09:36:11 -0600

    Initial commit

---
+++ CardRank/CardRank.h
@@ -0,0 +1,19 @@
+//
+//  CardRank.h
+//  CardRank
+//
+//  Created by Oscar Hernandez on 06/04/20.
+//  Copyright © 2020 Oscar Hernandez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardRank.
+FOUNDATION_EXPORT double CardRankVersionNumber;
+
+//! Project version string for CardRank.
+FOUNDATION_EXPORT const unsigned char CardRankVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardRank/PublicHeader.h>
+
+
---
+++ CardRankTests/CardRankTests.swift
@@ -0,0 +1,22 @@
+//
+//  CardRankTests.swift
+//  CardRankTests
+//
+//  Created by Oscar Hernandez on 06/04/20.
+//  Copyright © 2020 Oscar Hernandez. All rights reserved.
+//
+
+import XCTest
+@testable import CardRank
+
+class CardRankTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
